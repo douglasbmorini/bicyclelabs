103613006e2d97809c9aafe425c14474bd7eb1de
douglasbmorini <douglas_morini@hot.com>
2020-07-31 17:35:15 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menu from './components/Menu'
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      Coisas do mundo das bikes e mais!
+    </div>
+  );
+}
+
+export default App;
